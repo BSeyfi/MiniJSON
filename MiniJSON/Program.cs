19e5d3c0219002d36d9a31d0f33661a3a255502d
BSeyfi <42473071+BSeyfi@users.noreply.github.com>
2023-07-30 14:40:29 +0330

    Initial commit. Some files added.

---
+++ MiniJSON/Program.cs
@@ -0,0 +1,12 @@
+﻿using MiniJSON;
+
+var jsonString = "{ \"array\": [1.44,2,3], " +
+                 "\"object\": {\"key1\":\"value1\", \"key2\":256}, " +
+                 "\"string\": \"The quick brown fox \\\"jumps\\\" over the lazy dog \", " +
+                 "\"unicode\": \"\\u3041 Men\u00fa sesi\u00f3n\", " +
+                 "\"int\": 65536, " +
+                 "\"float\": 3.1415926, " +
+                 "\"bool\": true, " +
+                 "\"null\": null }";
+
+var dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
